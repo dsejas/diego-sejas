2d61fd1a75cc59d333a8695fe295d0366c8c3bbf
Diego Sejas-Viscarra <dsejas.math@pm.me>
2024-05-19 19:56:23 -0400

    Minimal working CSS

--- publish.el
+++ publish.el
@@ -55,18 +55,32 @@
                 (content "Diego Sejas-Viscarra")))
        (meta (@ (name "viewport")
                 (content "width=device-width, initial-scale=1, shrink-to-fit=no")))
+       (link (@ (rel "stylesheet")
+                (href "assets/css/site.css")))
+       (link (@ (rel "stylesheet")
+                (href "assets/css/fontawesome.min.css")))
+       (link (@ (rel "stylesheet")
+                (href "assets/css/solid.min.css")))
        (title ,title))
       (body
        (header
         (nav (@ (class "navbar"))
              (div (@ (class "navbar-left"))
-                  (a (@ (class "navbar-link") (href "/")) "")
-                  (a (@ (class "navbar-link") (href "/about")) "About me")
-                  (a (@ (class "navbar-link") (href "/projects")) "Projects")
-                  (a (@ (class "navbar-link") (href "/publications")) "Publications")
-                  (a (@ (class "navbar-link") (href "/cv")) "CV")
-                  (a (@ (class "navbar-link") (href "/news")) "News"))
-             (div (@ (class "navbar-right")))))
+                  (a (@ (class "navbar-link") (href "/"))
+                     (span (@ (class "fas fa-house navbar-icon")) ""))
+                  (a (@ (class "navbar-link") (href "/about"))
+                     (span (@ (class "fas fa-info-circle navbar-icon")) "") "About me")
+                  (a (@ (class "navbar-link") (href "/projects"))
+                     (span (@ (class "fas fa-tools navbar-icon")) "") "Projects")
+                  (a (@ (class "navbar-link") (href "/publications"))
+                     (span (@ (class "fas fa-pen-nib navbar-icon")) "") "Publications")
+                  (a (@ (class "navbar-link") (href "/cv"))
+                     (span (@ (class "fas fa-graduation-cap navbar-icon")) "") "CV")
+                  (a (@ (class "navbar-link") (href "/news"))
+                     (span (@ (class "fas fa-bullhorn navbar-icon")) "") "News"))
+             (div (@ (class "navbar-right"))
+                  (a (@ (class "navbar-link") (href "/en")) "English")
+                  (a (@ (class "navbar-link") (href "/es")) "Español"))))
        (main ,content))))))
 
 (defun dsv/site-template (content info)
@@ -102,7 +116,13 @@
               :base-directory "./content"
               :publishing-directory "./public"
               :publishing-function org-html-publish-to-html
-              :section-numbers nil)))
+              :section-numbers nil)
+            '("diego-sejas:assets"
+              :recursive t
+              :base-directory "./assets"
+              :base-extension "css\\|woff2"
+              :publishing-directory "./public/assets"
+              :publishing-function org-publish-attachment)))
 
 ;; Generate the site
 (org-publish-all t)
